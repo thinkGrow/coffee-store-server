96abd92111e75bec3247917a054dce253eb904ac
MaishaTahsin <rubaitreshad@gmail.com>
2025-09-03 01:32:25 +0600

    update last sign in time in db

--- index.js
+++ index.js
@@ -101,6 +101,28 @@ async function run() {
       res.send(result);
     });
 
+    app.patch("/users", async (req, res) => {
+      // console.log(req.body);
+      const { email, lastSignInTime } = req.body;
+      const filter = { email: email };
+      const updatedDoc = {
+        $set: {
+          lastSignInTime: lastSignInTime,
+        },
+      };
+
+      const result = await usersCollection.updateOne(filter, updatedDoc);
+
+      res.send(result);
+    });
+
+    app.delete("/users/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await usersCollection.deleteOne(query);
+      res.send(result);
+    });
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log(
